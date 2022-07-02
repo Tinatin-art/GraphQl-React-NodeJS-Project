f60e2188d6986e376e052dfa5a061c250d498c26
Tinatin <omurovatinatin@gmail.com>
2022-07-02 09:11:02 +0600

    graphql react project start

---
+++ client/src/App.js
@@ -0,0 +1,70 @@
+import { useMutation, useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { CREATE_USER } from './mutations/user';
+import { GET_ALL_USERS } from './query/user';
+
+const App = () => {
+
+  const {data, loading, refetch} = useQuery(GET_ALL_USERS);
+  // const {data: oneUser, loading: loadingOneUser} = useQuery(GET_ONE_USER, {
+  //   variables: {
+  //     id: 1
+  //   }
+  // });
+  const [newUser] = useMutation(CREATE_USER)
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('')
+  const [age, setAge] = useState(0)
+
+  useEffect(() => {
+    if (!loading) {
+      setUsers(data.getAllUsers)
+    }
+  }, [data, loading])
+
+  const addUser = (e) => {
+    e.preventDefault()
+    newUser({
+      variables: {
+        input: {
+          username,
+          age
+        }
+      }
+    }).then(({data}) => {
+      console.log(data);
+      setUsername('')
+      setAge(0)
+    })
+  }
+
+  const getAll = (e) => {
+    e.preventDefault()
+    refetch()
+  }
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <div>
+            <form>
+                <input value={username} onChange={e => setUsername(e.target.value)} type="text"/>
+                <input value={age} onChange={e => setAge(Number(e.target.value))} type="number"/>
+                <div className="btns">
+                    <button onClick={(e) => addUser(e)}>Создать</button>
+                    <button onClick={(e) => getAll(e)}>Получить</button>
+                </div>
+            </form>
+            <div>
+                {users.map((user, key) =>
+                    <div key={key} className="user">{user.id}  {user.username} {user.age}</div>
+                )}
+            </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/mutations/user.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_USER = gql`
+
+    mutation createUser($input: UserInput) {
+        createUser(input: $input), {
+            id
+            username
+            age
+        }
+    }
+    
+`
+
+// mutation createUser($input: UserInput) {
+    //     createUser(input: $input), {
+    //         id
+    //         username
+    //         age
+    //     }
+    // }
